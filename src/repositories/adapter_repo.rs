81bd644ed461276fb5b1cd4d5367d365a68324e1
Abigail Moore <abigailmmoore00@gmail.com>
2025-04-21 00:52:01 -0600

    Some reworks to adapter repo, added first tests

--- src/repositories/adapter_repo.rs
+++ src/repositories/adapter_repo.rs
@@ -1,17 +1,20 @@
 use std::any::{Any, TypeId};
+use std::fmt::{Debug, Formatter};
 use std::marker::PhantomData;
+use std::ops::Deref;
+use std::sync::Arc;
 use dashmap::DashMap;
 use crate::data::adapters::{Adapter, AdapterError};
 
 pub trait AdapterProvider {
     fn serialize<Domain, Serialized>(&self, domain: &Domain) -> Result<Serialized, AdapterRepoError>
     where
-        Domain: 'static,
-        Serialized: 'static;
+        Domain: Send + Sync + 'static,
+        Serialized: Send + Sync + 'static;
     fn deserialize<Serialized, Domain>(&self, serialized: &Serialized) -> Result<Domain, AdapterRepoError>
     where
-        Domain: 'static,
-        Serialized: 'static;
+        Domain: Send + Sync + 'static,
+        Serialized: Send + Sync + 'static;
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
@@ -26,10 +29,10 @@ pub struct AdapterRepository {
 }
 
 impl AdapterRepository {
-    pub fn register<Domain, Serialized, Adp>(&mut self, adapter: Adp)
+    pub fn register<Domain, Serialized, Adp>(&mut self)
     where
-        Domain: 'static,
-        Serialized: 'static,
+        Domain: Send + Sync + 'static,
+        Serialized: Send + Sync + 'static,
         Adp: Adapter<Serialized, Domain> + 'static + Send + Sync,
     {
         let adapter_type = AdapterType {
@@ -37,15 +40,35 @@ impl AdapterRepository {
             serialized: TypeId::of::<Serialized>(),
         };
 
+        let adapter = AdapterWrapper::<Domain, Serialized>::new::<Adp>();
+
         self.adapters.insert(adapter_type, Box::new(adapter));
+    }
+
+    #[cfg(test)]
+    fn get_adapter<Domain, Serialized>(&self) -> Option<AdapterWrapper<Domain, Serialized>>
+    where
+        Domain: Send + Sync + 'static,
+        Serialized: Send + Sync + 'static,
+    {
+        let adapter_type = AdapterType {
+            domain: TypeId::of::<Domain>(),
+            serialized: TypeId::of::<Serialized>(),
+        };
+
+        self.adapters.get(&adapter_type)
+            .as_deref()
+            .map(|a| {
+                a.downcast_ref::<AdapterWrapper<Domain, Serialized>>().unwrap()
+            }).cloned()
     }
 }
 
 impl AdapterProvider for AdapterRepository {
     fn serialize<Domain, Serialized>(&self, domain: &Domain) -> Result<Serialized, AdapterRepoError>
     where
-        Domain: 'static,
-        Serialized: 'static,
+        Domain: Send + Sync + 'static,
+        Serialized: Send + Sync + 'static,
     {
         let adapter_type = AdapterType {
             domain: TypeId::of::<Domain>(),
@@ -60,8 +83,8 @@ impl AdapterProvider for AdapterRepository {
 
     fn deserialize<Serialized, Domain>(&self, serialized: &Serialized) -> Result<Domain, AdapterRepoError>
     where
-        Domain: 'static,
-        Serialized: 'static,
+        Domain: Send + Sync + 'static,
+        Serialized: Send + Sync + 'static,
     {
         let adapter_type = AdapterType {
             domain: TypeId::of::<Domain>(),
@@ -76,20 +99,30 @@ impl AdapterProvider for AdapterRepository {
     }
 }
 
+#[derive(Debug)]
 struct AdapterWrapper<Domain, Serialized> {
-    adapter: Box<dyn AdapterObject<Domain, Serialized>>,
+    adapter: Arc<dyn AdapterObject<Domain, Serialized>>,
     _phantom: PhantomData<(Domain, Serialized)>,
+}
+
+impl<Domain, Serialized> Clone for AdapterWrapper<Domain, Serialized> {
+    fn clone(&self) -> Self {
+        Self {
+            adapter: self.adapter.clone(),
+            _phantom: PhantomData,
+        }
+    }
 }
 
 impl<Domain, Serialized> AdapterWrapper<Domain, Serialized> {
     fn new<Adp>() -> Self
     where
-        Domain: 'static,
-        Serialized: 'static,
+        Domain: Send + Sync + 'static,
+        Serialized: Send + Sync + 'static,
         Adp: Adapter<Serialized, Domain> + 'static + Send + Sync,
     {
         Self {
-            adapter: Box::new(AdapterObjectImpl::<Domain, Serialized, Adp>::new()),
+            adapter: Arc::new(AdapterObjectImpl::<Domain, Serialized, Adp>::new()),
             _phantom: PhantomData,
         }
     }
@@ -103,13 +136,15 @@ impl<Domain, Serialized> AdapterWrapper<Domain, Serialized> {
     }
 }
 
-trait AdapterObject<Domain, Serialized> {
+trait AdapterObject<Domain, Serialized>: Send + Sync + Debug {
     fn serialize(&self, domain: &Domain) -> Result<Serialized, AdapterRepoError>;
     fn deserialize(&self, serialized: &Serialized) -> Result<Domain, AdapterRepoError>;
 }
 
 struct AdapterObjectImpl<Domain, Serialized, Adp>
 where
+    Domain: Send + Sync + 'static,
+    Serialized: Send + Sync + 'static,
     Adp: Adapter<Serialized, Domain> + 'static + Send + Sync,
 {
     _phantom: PhantomData<(Domain, Serialized, Adp)>,
@@ -117,6 +152,8 @@ where
 
 impl<Domain, Serialized, Adp> AdapterObjectImpl<Domain, Serialized, Adp>
 where
+    Domain: Send + Sync + 'static,
+    Serialized: Send + Sync + 'static,
     Adp: Adapter<Serialized, Domain> + 'static + Send + Sync,
 {
     fn new() -> Self {
@@ -124,11 +161,25 @@ where
     }
 }
 
+impl<Domain, Serialized, Adp> Debug for AdapterObjectImpl<Domain, Serialized, Adp>
+where
+    Adp: 'static + Adapter<Serialized, Domain> + Send + Sync,
+    Adp::ConversionError: 'static + Send + Sync,
+    Domain: 'static + Send + Sync,
+    Serialized: 'static + Send + Sync,
+{
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", TypeId::of::<Adp>())
+    }
+}
+
 impl<Domain, Serialized, Adp> AdapterObject<Domain, Serialized> for AdapterObjectImpl<Domain, Serialized, Adp>
 where
+    Domain: Send + Sync + 'static,
+    Serialized: Send + Sync + 'static,
     Adp: Adapter<Serialized, Domain> + 'static + Send + Sync,
-    Adp::ConversionError: 'static,
-    Adp::SerializedConversionError: 'static,
+    Adp::ConversionError: Send + Sync + 'static,
+    Adp::SerializedConversionError: Send + Sync + 'static,
 {
     fn serialize(&self, domain: &Domain) -> Result<Serialized, AdapterRepoError> {
         Adp::serialize(domain).map_err(|e| AdapterRepoError::serialization_error(e))
@@ -168,4 +219,42 @@ impl AdapterRepoError {
 #[cfg(test)]
 mod test {
     use super::*;
+
+    struct Domain;
+    struct Serialized;
+
+    struct TestAdapter;
+
+    impl Adapter<Serialized, Domain> for TestAdapter {
+        type ConversionError = TestAdapterError;
+
+        fn deserialize(serialized: &Serialized) -> Result<Domain, Self::ConversionError> {
+            Ok(Domain)
+        }
+
+        fn serialize(domain: &Domain) -> Result<Serialized, Self::SerializedConversionError> {
+            Ok(Serialized)
+        }
+    }
+
+    #[derive(Debug, thiserror::Error)]
+    #[error("Test error")]
+    struct TestAdapterError;
+    impl AdapterError for TestAdapterError {}
+
+    #[test]
+    fn test_register_adapter() {
+        let mut repo = AdapterRepository::default();
+
+        // Given an adapter (TestAdapter)
+        // When I register it
+
+        repo.register::<Domain, Serialized, TestAdapter>();
+
+        // It should be added to the repository
+
+        let adapter = repo.get_adapter::<Domain, Serialized>();
+        
+        assert!(adapter.is_some());
+    }
 }
