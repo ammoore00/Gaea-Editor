38445034efe5617a067edc753d447ba134e7f84a
Abigail Moore <abigailmmoore00@gmail.com>
2025-05-06 11:59:47 -0600

    Added initial skeleton implementation of project de/ser

--- src/data/serialization/project.rs
+++ src/data/serialization/project.rs
@@ -1,4 +1,7 @@
+use std::collections::HashMap;
+use std::io::{Cursor, Read, Write};
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
+use zip::write::{ExtendedFileOptions, FileOptions};
 use crate::data::serialization::pack_info::PackInfo;
 
 #[derive(Debug, Clone, derive_new::new, getset::Getters)]
@@ -12,7 +15,14 @@ impl Serialize for Project {
     where
         S: Serializer
     {
-        todo!()
+        let buffer = Cursor::new(Vec::new());
+        let mut zip = zip::ZipWriter::new(buffer);
+
+        zip.start_file::<&str, ExtendedFileOptions>("pack.mcmeta", FileOptions::default()).map_err(serde::ser::Error::custom)?;
+        zip.write_all(serde_json::to_string(&self.pack_info).unwrap().as_bytes()).map_err(serde::ser::Error::custom)?;
+        
+        let zip_data = zip.finish().map_err(serde::ser::Error::custom)?;
+        serializer.serialize_bytes(&zip_data.into_inner())
     }
 }
 
@@ -21,7 +31,23 @@ impl<'de> Deserialize<'de> for Project {
     where
         D: Deserializer<'de>
     {
+        let zip_data: Vec<u8> = Deserialize::deserialize(deserializer)?;
+        let reader = Cursor::new(zip_data);
+        let mut zip = zip::ZipArchive::new(reader).map_err(serde::de::Error::custom)?;
+
+        let mut files = HashMap::new();
+
+        for i in 0..zip.len() {
+            let mut file = zip.by_index(i).map_err(serde::de::Error::custom)?;
+            let mut content = String::new();
+            file.read_to_string(&mut content)
+                .map_err(serde::de::Error::custom)?;
+
+            files.insert(file.name().to_string(), content);
+        }
+
         todo!()
+        //Ok(Project { files })
     }
 }
 
