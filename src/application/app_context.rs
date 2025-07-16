f6f3f0aa84b0071bfbca1f9c524669e9b47e7d87
Abigail Moore <abigailmmoore00@gmail.com>
2025-07-15 21:26:29 -0600

    Added undo service to app context

--- src/application/app_context.rs
+++ src/application/app_context.rs
@@ -3,15 +3,18 @@ use tokio::sync::RwLock;
 use crate::data::serialization::project::Project as SerializedProject;
 use crate::repositories::{adapter_repo, project_repo};
 use crate::services::project_service::{ProjectService, ProjectServiceProvider};
-use crate::services::{project_service, zip_service};
+use crate::services::{project_service, undo_service, zip_service};
+use crate::services::undo_service::{UndoProvider, UndoService};
 
 pub struct Uninitialized;
+pub struct ProjectServiceInitialized;
 pub struct Finalized;
 
 pub struct AppContextBuilder<State> {
     _phantom: std::marker::PhantomData<State>,
     
     project_service: Option<ProjectServiceContext>,
+    undo_service_context: Option<UndoServiceContext>,
 }
 
 impl Default for AppContextBuilder<Finalized> {
@@ -22,8 +25,11 @@ impl Default for AppContextBuilder<Finalized> {
             project_service::DefaultAdapterProvider::new()
         );
         
+        let undo_service = UndoService::new();
+        
         let self_ = AppContextBuilder::new()
-            .with_project_service(project_service);
+            .with_project_service(project_service)
+            .with_undo_service(undo_service);
         
         self_
     }
@@ -35,20 +41,36 @@ impl AppContextBuilder<Uninitialized> {
             _phantom: std::marker::PhantomData,
             
             project_service: None,
+            undo_service_context: None,
         }
     }
     
-    pub fn with_project_service<ProjectProvider, ZipProvider, AdapterProvider>(mut self, project_service: ProjectService<ProjectProvider, ZipProvider, AdapterProvider>) -> AppContextBuilder<Finalized>
+    pub fn with_project_service<ProjectProvider, ZipProvider, AdapterProvider>(self, project_service: ProjectService<ProjectProvider, ZipProvider, AdapterProvider>) -> AppContextBuilder<ProjectServiceInitialized>
     where
         ProjectProvider: project_repo::ProjectProvider + Send + Sync + 'static,
         ZipProvider: zip_service::ZipProvider<SerializedProject> + Send + Sync + 'static,
         AdapterProvider: adapter_repo::AdapterProvider + Send + Sync + 'static,
     {
         let project_service = Some(ProjectServiceContext::new(project_service));
-        AppContextBuilder::<Finalized> {
+        AppContextBuilder::<ProjectServiceInitialized> {
             project_service: project_service,
 
             _phantom: std::marker::PhantomData,
+            undo_service_context: None,
+        }
+    }
+}
+
+impl AppContextBuilder<ProjectServiceInitialized> {
+    pub fn with_undo_service(self, undo_service: impl UndoProvider + Send + Sync + 'static) -> AppContextBuilder<Finalized> {
+        let undo_service = Arc::new(RwLock::new(undo_service));
+        let undo_service_context = Some(UndoServiceContext(undo_service));
+        
+        AppContextBuilder::<Finalized> {
+            undo_service_context,
+            
+            _phantom: std::marker::PhantomData,
+            project_service: self.project_service,
         }
     }
 }
@@ -56,15 +78,18 @@ impl AppContextBuilder<Uninitialized> {
 impl AppContextBuilder<Finalized> {
     pub fn build(self) -> AppContext {
         let project_service_context = self.project_service.expect("Project service bypassed! Check type state pattern for errors");
+        let undo_service_context = self.undo_service_context.expect("Undo service bypassed! Check type state pattern for errors");
 
         AppContext {
             project_service_context,
+            undo_service_context,
         }
     }
 }
 
 pub struct AppContext {
     project_service_context: ProjectServiceContext,
+    undo_service_context: UndoServiceContext,
 }
 
 pub struct ProjectServiceContext(Arc<RwLock<dyn ProjectServiceProvider>>);
@@ -80,3 +105,5 @@ impl ProjectServiceContext {
         Self(Arc::new(RwLock::new(project_service)))
     }
 }
+
+pub struct UndoServiceContext(Arc<RwLock<dyn UndoProvider>>);
